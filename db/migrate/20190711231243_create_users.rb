823867cbf7dbd5b603c39f03574a25aef78c69d7
Dylan Clements <dclements@protonmail.com>
2019-07-12 11:00:26 -0400

    User migration, DBs created, Pry & bcrypt gems

--- db/migrate/20190711231243_create_users.rb
+++ db/migrate/20190711231243_create_users.rb
@@ -1,7 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-        
+        :username
+        :first_name
+        :last_name
+        :password_digest
       t.timestamps
     end
   end
