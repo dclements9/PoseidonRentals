38b07e9f4f80427ff6debc7b683bfa3cc7148e78
dclements9 <dclements@protonmail.com>
2019-07-13 13:58:45 -0400

    Associations, sessions g controller, home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'users#home'
+
   resources :equipment
   resources :reservations
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get '/login' => 'sessions#new'
 end
