ab4cda5d6dec79b83c6a37b2a49317546ebd8b2e
Dylan Clements <dclements@protonmail.com>
2019-07-16 10:51:55 -0400

    Reservation create w/ date,time,equipment,user & show

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -7,13 +7,21 @@ class ReservationsController < ApplicationController
     end
 
     def create
-        binding.pry
-        @equipment = Equipment.find_by(name: params[:equipment][:name])
-        @reservation = Reservation.new()
+        
+        @reservation = Reservation.new(reservation_params)
+        @reservation.user_id = session[:user_id]
+        @reservation.equipment_id  = params[:equipment][:id]
+
+        if @reservation.save
+            redirect_to reservation_path(@reservation)
+        else
+            render :new
+        end
     end
 
     def show
         @reservation = Reservation.find(params[:id])
+        @equipment = Equipment.find(@reservation.equipment_id) if @reservation.equipment_id
     end
 
     def edit
@@ -21,5 +29,11 @@ class ReservationsController < ApplicationController
     end
 
     def destroy
+    end
+
+    private
+
+    def reservation_params
+        params.require(:reservation).permit(:date, :start_time, :end_time)
     end
 end
