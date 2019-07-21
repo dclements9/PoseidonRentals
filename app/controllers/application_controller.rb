f92c3ea8bda0809786677b752248fbba63c83692
dclements9 <dclements@protonmail.com>
2019-07-21 14:31:13 -0400

    helper methods back to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
-    include ApplicationHelper
+    def reservation_date(reservation)
+        Reservation.date(reservation)
+    end
+
+    def reservation_start(reservation)
+        Reservation.start_time(reservation)
+    end
+
+    def reservation_end(reservation)
+        Reservation.end_time(reservation)
+    end
+
+    def find_user_if_
+        redirect_to root_path unless @user = User.find(session[:user_id])
+    end
+
+    def current_user
+        @current_user ||= User.find(session[:user_id])
+    end
+
+    def set_current_user
+        session[:user_id] = @user.id
+    end
 end
