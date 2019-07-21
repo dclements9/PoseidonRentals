3e8dab91d88b06d7d1b32212bdbb9d78d691826a
dclements9 <dclements@protonmail.com>
2019-07-21 16:40:36 -0400

    h tag Formatting, equipment new & edit actions & views, update, destroy & create actions, and links.

--- app/controllers/equipment_controller.rb
+++ app/controllers/equipment_controller.rb
@@ -1,12 +1,54 @@
 class EquipmentController < ApplicationController
-
+    before_action :is_admin?, only: [:edit, :update, :new, :create, :destroy]
+    before_action :find_equipment, only: [:update, :edit, :show, :destroy]
+    
     def index
         @equipment = Equipment.all
     end
 
     def show
-        @equipment = Equipment.find(params[:id])
     end
 
-    
+    def edit
+    end
+
+    def update
+        @equipment.update(equipment_params)
+        
+        redirect_to equipment_path(@equipment)
+    end
+
+    def new
+        @equipment = Equipment.new
+    end
+
+    def create
+        @equipment = Equipment.new(equipment_params)
+                
+        if @equipment.save
+            redirect_to equipment_path(@equipment)
+        else
+            render :new
+        end
+    end
+
+    def destroy
+        @equipment.destroy
+
+        redirect_to equipment_index_path
+    end
+
+    private
+
+    def equipment_params
+        params.require(:equipment).permit(:name, :category, :available, :cost)
+    end
+
+    def is_admin?
+        redirect_to equipment_index_path unless current_user.admin?
+    end
+
+    def find_equipment
+        @equipment = Equipment.find(params[:id])
+    end
 end
