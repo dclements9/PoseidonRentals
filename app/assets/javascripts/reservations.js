348b022902e52b1162cfdc1c66fcb696f4766d51
dclements9 <dclements@protonmail.com>
2019-09-16 16:15:41 -0400

    Clears displayReservationsDiv & minor user show page changes

--- app/assets/javascripts/reservations.js
+++ app/assets/javascripts/reservations.js
@@ -4,14 +4,12 @@ function getReservations(){
     fetch('http://localhost:3000/reservations.json')
     .then(resp => resp.json())
     .then(reservations => {
-        //Test Success Message
-        reservationsDiv.innerHTML += "Success";
+        reservationsDiv.innerHTML = '';
 
         for (var i = 0, len = reservations.length; i < len; ++i) {
             
             var objReservation = new Reservation(reservations[i]);
             reservationsDiv.innerHTML += objReservation.displayReservation();
-            
         }
     })
 }
@@ -29,14 +27,16 @@ class Reservation {
     }
 
     displayReservation(){
+        //TODO: proper converstion of timezone for displaying time.
+
         // If this.user_id == session[:user_id] - Access @user or session hash
+        // --- Should this happen in controller? For security API validations
 
         // Converts Ruby Date Type to formatted string date.
         var dateParts = this.date.split('-');
         var formatDate = new Date (dateParts[0], (dateParts[1] - 1), dateParts[2])
         
         //Converts Ruby Date (Time) Type to formatted string time.
-        //Still needs proper converstion of timezone. 
         
         var formatStart_time = (new Date(this.start_time)).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
 
@@ -55,7 +55,11 @@ class Reservation {
 function displayReservationForm(){
     let reservationForm = document.getElementById('reservation-form');
     let form = `
-        <label> Test </label> 
+        <form onsubmit="createReservation(); return false;">
+        <label> Test </label>
+        <input type="time">
+        <input type="submit" value="Create Reservation">
+        </form>
     `
     reservationForm.innerHTML = form;
 }
