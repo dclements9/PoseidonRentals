32358af23d3f1efa3ee4e030ae6d2efcb464ccc7
dclements9 <dclements@protonmail.com>
2019-09-17 16:37:37 -0400

    equipment.js - Class & beginning show reservations fetch. & equipment show reservations div/button

---
+++ app/assets/javascripts/equipment.js
@@ -0,0 +1,21 @@
+function getEquipment(){
+    let reservationsDiv = document.getElementById('equipment-reservations');
+    // Need equipment ID for below code.
+    let url = 'http://localhost:3000/equipment/' + this.id + '.json'
+
+    fetch(url)
+    .then(resp => resp.json())
+    .then(info => {
+        debugger;
+
+    })
+}
+
+class Equipment{
+    constructor(equipment){
+        this.name = equipment.id
+        this.category = equipment.category
+        this.available = equipment.available
+        this.cost = equipment.cost
+    }
+}
