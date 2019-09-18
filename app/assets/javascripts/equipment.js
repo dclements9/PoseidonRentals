2e165cd593b33dd2c12a043c0d75ed5ea23277fb
dclements9 <dclements@protonmail.com>
2019-09-18 10:27:11 -0400

    fetch equipment json from show page

--- app/assets/javascripts/equipment.js
+++ app/assets/javascripts/equipment.js
@@ -1,8 +1,9 @@
 function getEquipmentReservations(){
     let reservationsDiv = document.getElementById('equipment-reservations');
+    
     // Need equipment ID for below code.
-    let url = 'http://localhost:3000/equipment/' + this.id + '.json'
-
+    let url = window.location.href + ".json"
+    
     fetch(url)
     .then(resp => resp.json())
     .then(info => {
--- app/controllers/equipment_controller.rb
+++ app/controllers/equipment_controller.rb
@@ -11,6 +11,10 @@ class EquipmentController < ApplicationController
     end
 
     def show
+        respond_to do |format|
+            format.html
+            format.json {render json: @equipment}
+        end
     end
 
     def edit
