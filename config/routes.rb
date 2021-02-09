9debab02d972878245c5258d157e28393b7d8a27
dclements9 <dclements@protonmail.com>
2021-02-09 11:01:14 -0500

    get -> post

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,5 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
 
-  get '/auth/:provider/callback', to: 'sessions#oauth_create'
+  post '/auth/:provider/callback', to: 'sessions#oauth_create'
 end
